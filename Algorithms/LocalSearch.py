94e3af458ba850924d8b92c82ed14cb1093f94cb
21120447 <haonhat2729@gmail.com>
2023-10-29 23:16:55 +0700

    change local search logic

--- Algorithms/LocalSearch.py
+++ Algorithms/LocalSearch.py
@@ -1,76 +1,44 @@
-from Utils.utils import DDX, isValid2
-from constants import FOOD, MONSTER, WALL
+from Utils.utils import DDX, isValid2, isValid
+from constants import FOOD, MONSTER
 
 
-def update_heuristic(_map, start_row, start_col, current_row, current_col, N, M, depth, visited, _type, cost):
-    visited.append((current_row, current_col))
+def calc_heuristic(_map, visited, start_row, start_col, new_row, new_col, N, M, depth):
+    if abs(start_row - new_row) > 3 or abs(start_col - new_col) > 3 or depth > 9:
+        return 0
+    if not isValid2(_map, new_row, new_col, N, M):
+        return -float("inf")
+    if (new_row, new_col) in visited:
+        return 0
 
-    if depth < 0:
-        return
-    if (start_row, start_col) == (current_row, current_col):
-        return
+    visited.append((new_row, new_col))
 
-    point = 0
-    if _type == FOOD:
-        if depth == 2:
-            point = 35
-        if depth == 1:
-            point = 10
-        if depth == 0:
-            point = 5
-
-    elif _type == MONSTER:
-        if depth == 2:
-            point = float("-inf")
-        if depth == 1:
-            point = float("-inf")
-        if depth == 0:
-            point = -100
-
-    cost[current_row][current_col] += point
+    score = 0
+    if _map[new_row][new_col] == FOOD:
+        score = 1000 - depth * 10
+    elif _map[new_row][new_col] == MONSTER and depth <= 2:
+        return -float("inf")
+    else:
+        score = 100 - depth * 10
 
     for [d_r, d_c] in DDX:
-        new_row, new_col = current_row + d_r, current_col + d_c
-        if isValid2(_map, new_row, new_col, N, M) and (new_row, new_col) not in visited:
-            update_heuristic(_map, start_row, start_col, new_row, new_col, N, M, depth - 1, visited.copy(), _type, cost)
+        new_row_r, new_col_c = new_row + d_r, new_col + d_c
+        if isValid2(_map, new_row_r, new_col_c, N, M):
+            score += calc_heuristic(_map.copy(), visited.copy(), start_row, start_col, new_row_r, new_col_c, N, M,
+                                    depth + 1)
 
-
-def calc_heuristic(_map, start_row, start_col, current_row, current_col, N, M, depth, visited, cost, _visited):
-    visited.append((current_row, current_col))
-
-    if depth <= 0:
-        return
-
-    for [d_r, d_c] in DDX:
-        new_row, new_col = current_row + d_r, current_col + d_c
-        if isValid2(_map, new_row, new_col, N, M) and (new_row, new_col) not in visited:
-
-            sub_visited = []
-            if _map[new_row][new_col] == FOOD:
-                update_heuristic(_map, start_row, start_col, new_row, new_col, N, M, 2, sub_visited, FOOD, cost)
-            elif _map[new_row][new_col] == MONSTER:
-                update_heuristic(_map, start_row, start_col, new_row, new_col, N, M, 2, sub_visited, MONSTER,
-                                 cost)
-
-            calc_heuristic(_map, start_row, start_col, new_row, new_col, N, M, depth - 1, visited.copy(), cost,
-                           _visited)
-
-    cost[current_row][current_col] -= _visited[current_row][current_col]
+    return score
 
 
 def local_search(_map, start_row, start_col, N, M, _visited):
-    visited = []
-    cost = [[0 for _ in range(M)] for _ in range(N)]
-
-    calc_heuristic(_map, start_row, start_col, start_row, start_col, N, M, 3, visited, cost, _visited)
-
-    max_f = float("-inf")
-
-    result = []
+    res = []
+    visited = [(start_row, start_col)]
     for [d_r, d_c] in DDX:
         new_row, new_col = start_row + d_r, start_col + d_c
-        if cost[new_row][new_col] - _visited[new_row][new_col] > max_f and _map[new_row][new_col] != WALL:
-            max_f = cost[new_row][new_col] - _visited[new_row][new_col]
-            result = [new_row, new_col]
+        if isValid(_map, new_row, new_col, N, M):
+            res.append(([new_row, new_col], calc_heuristic(_map.copy(), visited.copy(), start_row, start_col, new_row,
+                                                           new_col, N, M, 1) - _visited[new_row][new_col]))
 
-    return result
+    res.sort(key=lambda k: k[1])
+    if len(res) > 0:
+        return res[-1][0]
+    return []
